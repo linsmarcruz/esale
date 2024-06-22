81413c0ffd065b28f62fd74fe5fd7866984618c2
linsmar.cruz.ext <linsmar.cruz.ext@infracommerce.com.br>
2024-06-22 10:44:39 -0300

    ✨ Create main file

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafios - Links para Perguntas</title>
+</head>
+
+<body>
+    <h1>Links para os Desafios</h1>
+    <ul>
+        <li><a href="challenge/questions/1">Desafio 1: Otimização de Rota de Entrega</a></li>
+        <li><a href="challenge/questions/2">Desafio 2: Rastreamento de Inventário</a></li>
+        <li><a href="challenge/questions/3">Desafio 3: Planejamento de Carga de Veículos</a></li>
+        <li><a href="challenge/questions/4">Desafio 4: Alocação de Veículos</a></li>
+        <li><a href="challenge/questions/5">Desafio 5: Previsão de Demanda</a></li>
+    </ul>
+</body>
+
+</html>
